37966897fd6c964ba7c7ecf7f2af36291186c024
seannam1218 <seannam1218@gmail.com>
2017-01-15 19:24:54 -0800

    Status bar fully functioning
    Replaced repeated codes with new functions named createDeleteIcon() and
    createStatusIcon(). Everything else fully working. trailing spaces
    removed + tabs converted to spaces

--- Todo.js
+++ Todo.js
@@ -1,4 +1,4 @@
-	
+
         //DEFINITIONS
         var bullet = "- ";
 
@@ -7,127 +7,144 @@
         var label = $("<label>").append("Add an item <br>").append(this.input);
 
         var Icon = function(alt, src) {
-			this.alt = alt;
+            this.alt = alt;
             this.src = src;
         }
-		
-		var deleteIconAlt = "delete item";
-        var deleteIconSrc = "https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_delete_48px-128.png";
-		
-		var statusIconAlt = ["pending", "started", "finished", "stopped"];
-		var statusIconSrc = [
-			"https://d30y9cdsu7xlg0.cloudfront.net/png/99630-200.png",   //pending
-			"https://d30y9cdsu7xlg0.cloudfront.net/png/42732-200.png",	//started
-			"https://cdn1.iconfinder.com/data/icons/materia-arrows-symbols-vol-6/24/018_237_ok_check_done_finished-128.png",	//finished
-			"https://cdn2.iconfinder.com/data/icons/player-rounded-set/154/player-stop-frame-video-music-256.png" //stopped
-		];
 
-		
+        var $deleteIcon;
+
+        var createDeleteIcon = function() {
+            $deleteIcon = $("<img>")
+                .attr("class", "deleteIcon")
+                .attr("src", deleteIconSrc)
+                .attr("alt", deleteIconAlt)
+                .on("click", function(event) {
+                    event.preventDefault();
+                    var $target = $(event.currentTarget).closest("p");
+                    $target.remove();
+                });
+
+        }
+
+        var deleteIconAlt = "delete item";
+        var deleteIconSrc = "https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_delete_48px-128.png";
+
+        var $statusIcon;
+        var items = [];
+        var iconClasses = [];
+        var $p;
+        var statusIndex;
+
+        var createStatusIcon = function(i) {
+            if (!iconClasses || iconClasses.length == 0) {
+                return
+            } else {
+				console.log(iconClasses)
+                statusIndex = iconClasses[i].substr(-1);
+                $p = $("<p>").attr("class", "status" + statusIndex);
+                $statusIcon = $("<img>")
+                    .attr("class", iconClasses[i])
+                    .attr("src", statusIconSrc[statusIndex])
+                    .attr("alt", statusIconAlt[statusIndex])
+                    .on("click", function(event) {
+                        event.preventDefault();
+                        var $target = $(event.currentTarget);
+                        var $targetP = $(event.currentTarget).closest("p");
+                        var index = $targetP.attr("class").substr(-1);
+                        if ($targetP.attr("class") == "status3") {
+                            index = 0;
+                            $targetP.removeClass("status3");
+                            $targetP.attr("class", "status0");
+                            $target.attr("src", statusIconSrc[index])
+                                .attr("alt", statusIconAlt[index]);
+
+                        } else {
+                            $targetP.removeClass("status" + index)
+                            index ++;
+                            $targetP.attr("class", "status" + index);
+                            $target.attr("src", statusIconSrc[index])
+                                .attr("alt", statusIconAlt[index]);
+                        }
+                })
+            }
+
+        }
+
+        var statusIconAlt = ["pending", "started", "finished", "stopped"];
+        var statusIconSrc = [
+            "https://d30y9cdsu7xlg0.cloudfront.net/png/99630-200.png",   //pending
+            "https://d30y9cdsu7xlg0.cloudfront.net/png/42732-200.png",  //started
+            "https://cdn1.iconfinder.com/data/icons/materia-arrows-symbols-vol-6/24/018_237_ok_check_done_finished-128.png",    //finished
+            "https://cdn2.iconfinder.com/data/icons/player-rounded-set/154/player-stop-frame-video-music-256.png" //stopped
+        ];
+
+
         //LOAD DATA// make items array corresponding to what is stored in localStorage
         var load = function() {
-			var savedTexts = JSON.parse(localStorage.getItem("items"));
-			var savedIconClasses = JSON.parse(localStorage.getItem("iconClasses"));
-        
-			if (!savedTexts) {
-				var items = [];
-				var IconClasses = [];
-			} else {
-				var items = savedTexts;
-				var iconClasses = savedIconClasses;
-				for (var i = 0; i < savedTexts.length; i++) {
-					var statusIndex = iconClasses[i].substr(-1);
-					var $p = $("<p>").attr("class", "status" + statusIndex);
-					var $statusIcon = $("<img>")
-						.attr("class", iconClasses[i])
-						.attr("src", statusIconSrc[statusIndex])
-						.attr("alt", statusIconAlt[statusIndex])
-						.on("click", function(event) {
-							event.preventDefault();
-							var $target = $(event.currentTarget);
-							var $targetP = $(event.currentTarget).closest("p");
-							console.log($targetP);
-							var index = $targetP.attr("class").substr(-1);
-							console.log("clicked");
-							if ($targetP.attr("class") == "status3") {
-								index = 0;
-								$targetP.removeClass("status3");
-								$targetP.attr("class", "status0");
-								$target.attr("src", statusIconSrc[index])
-									.attr("alt", statusIconAlt[index]);		
-								
-							} else {
-								$targetP.removeClass("status" + index)
-								index ++;
-								$targetP.attr("class", "status" + index);
-								$target.attr("src", statusIconSrc[index])
-									.attr("alt", statusIconAlt[index]);
-							}
-					
-							console.log($targetP.attr("class"));
-						});
-					
-					var $deleteIcon = $("<img>")
-						.attr("class", "deleteIcon")
-						.attr("src", deleteIconSrc)
-						.attr("alt", deleteIconAlt)
-						.on("click", function(event) {
-							event.preventDefault();
-							var $target = $(event.currentTarget).closest("p");
-							$target.remove();
-						});
-					var $answerP = $p.append($statusIcon).append(items[i]).append($deleteIcon).append($("<br>"));
-					$(".list").append($answerP);
-				}
-			}
-		}
+            var savedTexts = JSON.parse(localStorage.getItem("items"));
+            var savedIconClasses = JSON.parse(localStorage.getItem("iconClasses"));
+
+            if (!savedTexts || savedTexts.length == 0) {
+                items = [];
+                iconClasses = [];
+                console.log("no saved files found")
+            } else {
+                console.log("loading saved files")
+                items = savedTexts;
+                iconClasses = savedIconClasses;
+                console.log(iconClasses);
+
+                for (var i = 0; i < items.length; i++) {
+                    createStatusIcon(i)
+                    createDeleteIcon();
+
+                    console.log($statusIcon)
+                    console.log(items[i])
+                    console.log($deleteIcon)
+                    console.log($p)
+
+                    var $answerP = $p.append($statusIcon).append(items[i]).append($deleteIcon).append($("<br>"));
+                    $(".list").append($answerP);
+                }
+
+            }
+        }
 
         var $mainForm = $("<form>").attr("class", "main-form").append(label).append(button).attr("input", input)
             .on("submit", function(event) {
-				var $p = $("<p>").attr("class", "status0")
+                var $p = $("<p>").attr("class", "status0")
                 event.preventDefault();
                 $(".dialog").text("");
                 var inputAnswer = $(this).find("input").val();
                 if (inputAnswer !== "") {
-					var $statusIcon = $("<img>")
-					.attr("class", "status0")
+                    var $statusIcon = $("<img>")
+                    .attr("class", "status0")
                     .attr("src", statusIconSrc[0])
                     .attr("alt", statusIconAlt[0])
-					.on("click", function(event) {
-						event.preventDefault();
-						var $target = $(event.currentTarget);
-						var $targetP = $(event.currentTarget).closest("p");
-						console.log($targetP);
-						var index = $targetP.attr("class").substr(-1);
-						console.log("clicked");
-						
-						if ($targetP.attr("class") == "status3") {
-							index = 0;
-							$targetP.removeClass("status3")
-							$targetP.attr("class", "status0");
-							$target.attr("src", statusIconSrc[index])
-								.attr("alt", statusIconAlt[index]);		
-						} else {
-							$targetP.removeClass("status" + index)
-							index ++;
-							$targetP.attr("class", "status" + index);
-							$target.attr("src", statusIconSrc[index])
-								.attr("alt", statusIconAlt[index]);
-						}
-						
-						console.log($targetP.attr("class"));
-					});
-					           
-                    var $deleteIcon = $("<img>")
-						.attr("class", "deleteIcon")
-                        .attr("src", deleteIconSrc)
-                        .attr("alt", deleteIconAlt)
-                        .on("click", function(event) {
-                            event.preventDefault();
-                            var $targetP = $(event.currentTarget).closest("p");
-                            $targetP.remove();
-                            }
-                        )
-                    
+                    .on("click", function(event) {
+                        event.preventDefault();
+                        var $target = $(event.currentTarget);
+                        var $targetP = $(event.currentTarget).closest("p");
+                        var index = $targetP.attr("class").substr(-1);
+
+                        if ($targetP.attr("class") == "status3") {
+                            index = 0;
+                            $targetP.removeClass("status3")
+                            $targetP.attr("class", "status0");
+                            $target.attr("src", statusIconSrc[index])
+                                .attr("alt", statusIconAlt[index]);
+                        } else {
+                            $targetP.removeClass("status" + index)
+                            index ++;
+                            $targetP.attr("class", "status" + index);
+                            $target.attr("src", statusIconSrc[index])
+                                .attr("alt", statusIconAlt[index]);
+                        }
+
+                    });
+
+                    createDeleteIcon();
+
                     var $answerP = $p.append($statusIcon).append(bullet).append(inputAnswer).append($deleteIcon).append($("<br>"))
                     $(".list").append($answerP);
                 }
@@ -136,22 +153,17 @@
         $(".save-all").on("click", function(event) {
             event.preventDefault();
             items = [];
-			iconClasses = [];
+            iconClasses = [];
             $("p").each(function() {
                 var text = $(this).text();
-				console.log("text = " + text)
                 items.push(text);
-				console.log("this.html = " + $(this).html())
-				var iconClass = $(this).attr("class");
-				console.log("iconClass = " + iconClass)
-				iconClasses.push(iconClass);
+                var iconClass = $(this).attr("class");
+                iconClasses.push(iconClass);
                 }
             );
-			console.log("items list = " + items)
-			console.log("iconclass list = " + iconClasses)
-			
+
             localStorage.setItem("items", JSON.stringify(items));
-			localStorage.setItem("iconClasses", JSON.stringify(iconClasses));
+            localStorage.setItem("iconClasses", JSON.stringify(iconClasses));
         });
 
         $(".delete-all").on("click", function(event) {
@@ -161,7 +173,7 @@
                 var yesButton = $("<button>").attr("type", "submit").text("hell yeah")
                     .on("click", function(event) {
                         items = [];
-						iconClasses = [];
+                        iconClasses = [];
                         $(".list").html("");
                         $(".dialog").html("");
                         }
@@ -175,6 +187,51 @@
                 $(".dialog").append("Are you sure you want to delete everything?").append("<br>").append(yesButton).append(noButton);
             }
         });
+
+        $(".reorder").on("click", function(event) {
+            event.preventDefault();
+            items = [];
+            iconClasses = [];
+
+            $("p").each(function() {
+                var text = $(this).text();
+                items.push(text);
+                var iconClass = $(this).attr("class");
+                iconClasses.push(iconClass);
+            });
+
+            var reorder = function() {
+                for (var i = 0; i < iconClasses.length-1; i++) {
+                    var statusIndex = iconClasses[i].substr(-1);
+                    var statusIndexNext = iconClasses[i+1].substr(-1);
+
+                    if (statusIndex > statusIndexNext) {
+                        var temp = iconClasses[i+1];
+                        iconClasses[i+1] = iconClasses[i];
+                        iconClasses[i] = temp;
+
+                        var temp = items[i+1];
+                        items[i+1] = items[i];
+                        items[i] = temp;
+                        console.log("swapping complete")
+
+                        reorder();
+                    }
+                }
+            }
+
+            reorder();
+            $(".list").html("");
+
+            for (var i = 0; i < items.length; i++) {
+                createStatusIcon(i)
+                createDeleteIcon();
+
+                var $answerP = $p.append($statusIcon).append(items[i]).append($deleteIcon).append($("<br>"));
+                $(".list").append($answerP);
+            }
+        })
+
 
         //MAIN CODE//
         $mainForm.appendTo($(".form-space"));
